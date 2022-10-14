e2d395a9964576e7c2340ab882df7c623bf4ef4b
Bryanst11 <mendezbryan88@gmail.com>
2022-10-13 19:45:13 -0500

    prueba1

--- ERPangular/src/app/app-routing.module.ts
+++ ERPangular/src/app/app-routing.module.ts
@@ -5,16 +5,19 @@ import { RouterModule, Routes } from '@angular/router';
 import { ContactoComponent } from './components/contacto/contacto.component';
 import { IngresoComponent } from './components/ingreso/ingreso.component';
 import { InicioComponent } from './components/inicio/inicio.component';
+import { ListarPersonalComponent } from './components/listar-personal/listar-personal.component';
 import { NotFoundComponent } from './components/not-found/not-found.component';
-import { InicioDashboardComponent} from './components/inicio-dashboard/inicio-dashboard.component'
+import { RegistroPersonalComponent } from './components/registro-personal/registro-personal.component';
+
 
 
 const routes: Routes = [
     {path:'', component: InicioComponent},
     {path:'ingreso', component: IngresoComponent},
     {path:'contacto', component: ContactoComponent},
+    {path:'listar-personal', component: ListarPersonalComponent},
+    {path:'registro-personal', component: RegistroPersonalComponent},
     {path:'404',component:NotFoundComponent},
-    {path:'inicio-dashboard',component:InicioDashboardComponent},
     {path:'**', redirectTo:'404', pathMatch: 'full'}, //cette ligne tjs a la fin de ce bloc
 
 ];
--- ERPangular/src/app/app.module.ts
+++ ERPangular/src/app/app.module.ts
@@ -13,8 +13,8 @@ import { InicioComponent } from './components/inicio/inicio.component';
 import { IngresoComponent } from './components/ingreso/ingreso.component';
 import { ContactoComponent } from './components/contacto/contacto.component';
 import { NotFoundComponent } from './components/not-found/not-found.component';
-import { InicioDashboardComponent } from './components/inicio-dashboard/inicio-dashboard.component';
-
+import { ListarPersonalComponent } from './components/listar-personal/listar-personal.component';
+import { RegistroPersonalComponent } from './components/registro-personal/registro-personal.component';
 
 
 @NgModule({
@@ -26,8 +26,8 @@ import { InicioDashboardComponent } from './components/inicio-dashboard/inicio-d
     IngresoComponent,
     ContactoComponent,
     NotFoundComponent,
-    InicioDashboardComponent,
-
+    ListarPersonalComponent,
+    RegistroPersonalComponent,
   ],
   imports: [
     BrowserModule,
