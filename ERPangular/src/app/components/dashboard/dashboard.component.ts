8bc4eb265d139dde8e8e4ed5cc1bebdbab5272eb
Bryanst11 <mendezbryan88@gmail.com>
2022-10-27 09:01:24 -0500

    tsdash

--- ERPangular/src/app/components/dashboard/dashboard.component.ts
+++ ERPangular/src/app/components/dashboard/dashboard.component.ts
@@ -15,20 +15,6 @@ export class DashboardComponent implements OnInit {
 
   ngOnInit(): void {
 
-    this.dropdownList = [
-      { item_id: 1, item_text: 'Mumbai' },
-      { item_id: 2, item_text: 'Bangaluru' },
-      { item_id: 3, item_text: 'Pune' },
-      { item_id: 4, item_text: 'Navsari' },
-      { item_id: 5, item_text: 'New Delhi' }
-    ];
-    this.dropdownSettings = {
-      singleSelection: false,
-      idField: 'item_id',
-      textField: 'item_text',
-      unSelectAllText: 'UnSelect All',
-      itemsShowLimit: 5,
-      allowSearchFilter: true
-    };
+
+    }
   }
-}
