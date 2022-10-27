9b1d4d04b8a7a9177cb2f5e4eae607e1aa9fd141
Bryanst11 <mendezbryan88@gmail.com>
2022-10-27 11:39:19 -0500

    final

--- ERPangular/src/app/components/correos/correos.component.ts
+++ ERPangular/src/app/components/correos/correos.component.ts
@@ -1,21 +1,24 @@
 import { Component, OnInit } from '@angular/core';
 import { ContactoService } from 'src/app/services/contacto.service';
 import { Contacto } from 'src/app/models/contactoModel';
-
+import Swal from 'sweetalert2';
 
 @Component({
     selector: 'app-correos',
     templateUrl: './correos.component.html',
     styleUrls: ['./correos.component.css']
 })
+
+
 export class CorreosComponent implements OnInit {
 
     // variable qui s'appuie sur l'import contactoModelo et qui mettra tout dans un array
-    listarCorreos: Contacto[]=[];
+    listarCorreos: Contacto[] = [];
 
     constructor(private servicioContacto: ContactoService) { }
 
     ngOnInit(): void {
+        this.obtenerContactos()
 
     }
 
@@ -27,6 +30,34 @@ export class CorreosComponent implements OnInit {
             console.log(error);
         })
     }
-}
 
-console.log();
+    deleteContacto(id:any){
+        Swal.fire({
+            title: "Desea borrar el correo?",
+            text:"Con cuidado porque el proceso es irreversible",
+            icon:"warning",
+            showCancelButton: true,
+            confirmButtonColor: '#3085d6',
+            cancelButtonColor: '#d33',
+            confirmButtonText:'Si borrarlo'
+        }).then ((result)=>{
+            if(result.isConfirmed){
+                this.servicioContacto.deleteContacto(id).subscribe((data) => {
+                    this.obtenerContactos()
+                    Swal.fire({
+                        title: "Correo eliminado correctamente",
+                        icon:"success",
+                    }
+
+
+
+                    )
+                }, (error) => {
+                    console.log(error);
+                })
+
+            }
+        })
+    }
+
+}
